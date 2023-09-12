5a585aabd69afa2519b3ce78fba2864c544448da
Alok Singh Bais <74749762+Alok123-singh@users.noreply.github.com>
2023-09-12 05:47:41 +0000

    dataypes updated

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,5 +1,19 @@
 "use strict"; // use newer version of javascript thourgout this file
 
+let myFirstDate = new Date();
+// console.log(myFirstDate);
+// console.log(typeof myFirstDate); // Object
+
+let mySecondDate = Date();
+// console.log(mySecondDate);
+// console.log(typeof mySecondDate); // String
+
+let myTimeStamp1 = Date.now();
+// console.log(myTimeStamp1);
+// console.log(typeof myTimeStamp1); // Number
+
+let myTimeStamp2 = Date.now();
+// console.log(myTimeStamp2 - myTimeStamp1); // calculating difference between timestamps 
 
 
 
