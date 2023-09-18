e00e41ab08f64233c676dda9fda68d1410e7cf4b
Alok Singh Bais <74749762+Alok123-singh@users.noreply.github.com>
2023-09-18 03:58:53 +0000

    added asyn await inside practice

--- 11_practice/01_pr/script.js
+++ 11_practice/01_pr/script.js
@@ -1,18 +1,37 @@
-const anchor = document.querySelector('#anchor');
+// const anchor = document.querySelector('#anchor');
 
-let cnt = 0;
-anchor.addEventListener('click',function(e){
-    anchor.innerHTML = `clicked ${cnt} times`;
-    const newAnchor = document.createElement('a');
-    newAnchor.setAttribute('href','https://www.youtube.com')
-    newAnchor.setAttribute('target','_blank');
-    anchor.parentNode.appendChild(newAnchor);
-    newAnchor.style.color = 'yellow';
-    newAnchor.innerHTML = "Youtube";
-    console.log(cnt);
-    cnt++;
-})
+// let cnt = 0;
+// anchor.addEventListener('click',function(e){
+//     anchor.innerHTML = `clicked ${cnt} times`;
+//     const newAnchor = document.createElement('a');
+//     newAnchor.setAttribute('href','https://www.youtube.com')
+//     newAnchor.setAttribute('target','_blank');
+//     anchor.parentNode.appendChild(newAnchor);
+//     newAnchor.style.color = 'yellow';
+//     newAnchor.innerHTML = "Youtube";
+//     console.log(cnt);
+//     cnt++;
+// })
 
 
+async function one(){
+    const promise = new Promise(function(resolve,reject){
+        const error = false;
+        if(!error){
+            setTimeout(function(){
+                console.log('task executed');
+            },2000);
+            resolve('Resolved');
+        }
+        else{
+            reject('Rejected');
+        }
+    });
+
+    let response = await promise;
+    console.log(response);
+}
+
+one();
 
 
